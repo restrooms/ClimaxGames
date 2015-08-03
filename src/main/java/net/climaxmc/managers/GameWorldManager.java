cf9ce1a918c0ef6e4fdd5382673fbc4092e6964d
computerwizjared <computerwizjared@hotmail.com>
2015-08-03 16:53:50 -0500

    Unzip from maps folder

--- src/main/java/net/climaxmc/managers/GameWorldManager.java
+++ src/main/java/net/climaxmc/managers/GameWorldManager.java
@@ -1,5 +1,8 @@
 package net.climaxmc.managers;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.lang.math.RandomUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
@@ -9,16 +12,43 @@ public class GameWorldManager extends Manager {
     private File mapsFolder;
 
     GameWorldManager() {
-        initializeWorldFolders();
+        initializeMapFolders();
+        loadMaps();
     }
 
-    private void initializeWorldFolders() {
+    private void initializeMapFolders() {
         File root = new File((System.getProperty("os.name").startsWith("Windows") ? "C:" : "") + File.separator + "maps");
         mapsFolder = new File(root.getPath() + File.separator + manager.getGame().getName());
 
         if (mapsFolder.mkdirs()) {
             plugin.getLogger().info("Created maps folder at " + mapsFolder.getPath());
         }
+    }
+
+    private void loadMaps() {
+        File[] mapFiles = mapsFolder.listFiles();
+
+        if (mapFiles == null || mapFiles.length <= 0) {
+            plugin.getLogger().severe("No maps available to load!");
+            return;
+        }
+
+        int mapId = RandomUtils.nextInt(mapFiles.length);
+        int i = 0;
+        for (File mapZipFile : mapFiles) {
+            if (!mapZipFile.getName().endsWith(".zip")) {
+                continue;
+            }
+
+            if (i++ == mapId) {
+                try {
+                    new ZipFile(mapZipFile).extractAll("GameMap");
+                } catch (ZipException e) {
+                    plugin.getLogger().severe("Could not extract zip file " + mapZipFile.getPath());
+                }
+            }
+        }
+
     }
 
     @EventHandler
