24c5de4d3aea622e4b8d9f6aed8fab00dcc4c95a
computerwizjared <computerwizjared@hotmail.com>
2015-08-12 21:17:45 -0500

    Gotta go... updater does not work yet

--- src/main/java/net/climaxmc/updater/Updater.java
+++ src/main/java/net/climaxmc/updater/Updater.java
@@ -3,16 +3,21 @@ package net.climaxmc.updater;
 import net.climaxmc.ClimaxGames;
 import net.climaxmc.events.GameStateChangeEvent;
 import net.climaxmc.game.Game;
+import net.climaxmc.utilities.F;
+import net.climaxmc.utilities.UtilPlugin;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.*;
 
 public class Updater implements Listener {
     private ClimaxGames plugin;
 
     public Updater(ClimaxGames plugin) {
         this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
@@ -22,12 +27,37 @@ public class Updater implements Listener {
         }
 
         boolean windows = System.getProperty("os.name").startsWith("Windows");
-        File file = new File((windows ? "C:" : File.separator + "tmp") + File.separator + "update");
+        File file = new File((windows ? "C:" : File.separator + "tmp") + File.separator + "update" + File.separator);
+        File oldFile = new File("plugins" + File.separator + "ClimaxGames.jar");
 
-        if (!file.exists()) {
+        if (!file.exists() || !oldFile.exists()) {
             return;
         }
 
+        try {
+            if (DigestUtils.md5Hex(new FileInputStream(file)).equals(DigestUtils.md5Hex(new FileInputStream(oldFile)))) {
+                return;
+            }
+        } catch (IOException e) {
+            plugin.getLogger().severe("Could not compare update to original! " + e.getMessage());
+        }
 
+        plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
+            plugin.getServer().broadcastMessage(F.message("Updater", "Reloading in 5 seconds for an update..."));
+
+            plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
+                UtilPlugin.unload(plugin);
+
+                try {
+                    Files.copy(Paths.get(file.getPath()), Paths.get(plugin.getServer().getUpdateFolderFile().getParentFile().getPath() + File.separator + "ClimaxGames.jar"), StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    plugin.getLogger().severe("Could not copy update! " + e.getMessage());
+                }
+
+                UtilPlugin.load("ClimaxGames");
+
+                plugin.getServer().broadcastMessage(F.message("Updater", "Reload complete!"));
+            }, 100);
+        }, 40);
     }
 }
